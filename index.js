741fc45b9d1e269341d8eb889d87bbaa8500328c
clock1222 <101945804+clock1222@users.noreply.github.com>
2023-08-23 19:47:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ function timingup(){
   document.getElementById("timingH").innerText = NTH;
   document.getElementById("timingM").innerText = NTM;
   document.getElementById("timingS").innerText = NTS;
-  //Times
-  if(new URLSearchParams(window.parent.location.search).get('ErrorTimes') == null){
-    document.getElementById("times").innerText = 0;
-  }else{
-    document.getElementById("times").innerText = (parseInt(new URLSearchParams(window.parent.location.search).get('ErrorTimes'))+parseInt(new URLSearchParams(window.parent.location.search).get('OkTimes'))).toString().padStart(2, '0');
-  }
-  document.getElementById("errortimes").innerText = new URLSearchParams(window.parent.location.search).get('ErrorTimes').toString().padStart(2, '0');
-  document.getElementById("oktimes").innerText = new URLSearchParams(window.parent.location.search).get('OkTimes').toString().padStart(2, '0');
   //Oking
   if(parseInt(new URLSearchParams(window.parent.location.search).get('OkTimes'))>1){
     document.getElementById("txt").innerText = "與伺服器連線成功！";
