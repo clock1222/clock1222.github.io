98f425e140273e6d70d7ad4ff2d4b393e268458d
clock1222 <101945804+clock1222@users.noreply.github.com>
2023-08-23 21:54:57 +0800

    Update iframeURL.js

--- iframeURL.js
+++ iframeURL.js
@@ -2,7 +2,10 @@ if (window.frameElement != null) {
     if(window.location.hostname != "clock1222.github.io"){
         alert(window.location.hostname)
         document.write("<p style='font-family:Noto Serif TC,serif;font-weight: bold;font-size:20px;z-index:1000000000000;position:fixed;top:0px;left:0px;background-color:red;color:white;padding-left:10px;padding-right:10px;padding-top:2px;padding-bottom:2px;margin:0;'>您目前並不是使用原網址(<a href='https://1514.myqnapcloud.com'>https://1514.myqnapcloud.com</a>)瀏覽本網站，請務必小心個資安全與法律責任，感謝您的配合。</p>");
+    }else{
+                alert(window.location.hostname)
     }
 } else {
+            alert(window.location.hostname)
     document.write("<p style='font-family:Noto Serif TC,serif;font-weight: bold;font-size:20px;z-index:1000000000000;position:fixed;top:0px;left:0px;background-color:red;color:white;padding-left:10px;padding-right:10px;padding-top:2px;padding-bottom:2px;margin:0;'>您目前並不是使用原網址(<a href='https://1514.myqnapcloud.com'>https://1514.myqnapcloud.com</a>)瀏覽本網站，請務必小心個資安全與法律責任，感謝您的配合。</p>");
 } 
